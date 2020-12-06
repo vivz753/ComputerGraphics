d961a7b21f6459dcd5f46a99a22023ddf0d19780
Vivian Leung <vivz753@gmail.com>
2020-12-06 14:32:19 -0800

    add hit_sphere() ray-sphere intersection function, make Rasterize() dynamic w/ params for imageWidth & imageHeight, add PrintProgress() function for rendering progress

--- CSE168/hw1/RayTracer/src/Main.cpp
+++ CSE168/hw1/RayTracer/src/Main.cpp
@@ -4,25 +4,55 @@
 #include "vec3.h"
 #include "ray.h"
 
-using namespace std;
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	// ray from center of sphere to eye
+	vec3 oc = r.origin() - center;
+	// expanded from the formula x^2 + y^2 + z^2 = r^2
+	// modified to be (P-C)^2 = r^2 where P = A + tb where A is the ray origin and t is a real number (double) and b is the ray direction
+	// so (A + tb - C)^2 = r^2 can be expanded to a quadratic equation t^2*b^2 + 2tb(A-C) + (A-C)^2 = r^2 which is in the form of Ax^2 + Bx + C = 0 if we move r^2 to the left side
+	// t is the unknown, so it represents the x in the quadratic eq
+	// a = t^2*b^2 or tb*tb since tb is the direction in P = A + tb
+	auto a = dot(r.direction(), r.direction());
+	// b = 2*tb*(A-C)
+	auto b = 2.0 * dot(oc, r.direction());
+	// c = (A-C)*(A-C) - r^2
+	auto c = dot(oc, oc) - radius * radius;
+	// uses the quadratic formula's b2-4ac to find real roots
+	auto discriminant = b * b - 4 * a * c;
+	// return real roots
+	return discriminant > 0;
+}
 
 color ray_color(const ray& r) {
+	// if ray hits sphere at 0, 0, -1 w/ a radius of 0.5, return red color
+	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+		return color(1, 0, 0);
+	}
+	// else return the gradient background sky
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel) {
+void PrintProgress(int i, int j, int imageWidth, int imageHeight, int progressArray[]) {
+	float progress = static_cast<float>((i * (imageHeight)+((j - imageHeight + 1.0) * -1)));
+	float total = static_cast<float> ((imageHeight) * (imageWidth));
+	int percent = static_cast<float>(progress / total * 100);
+	if (progressArray[percent] != 1) {
+		std::cout << "Rendering: " << percent << "%" << std::endl;
+		progressArray[percent] = 1;
+	}
+}
 
-	//const int WIDTH(50), HEIGHT(50), BPP(24);
+void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel) {
 
 	const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
 
+	// FreeImage setup
 	FreeImage_Initialise();
-
 	FIBITMAP* bitmap = FreeImage_Allocate(imageWidth, imageHeight, bitsPerPixel);
 	RGBQUAD *freeimage_color = new RGBQUAD();
-
 	if (!bitmap)
 		exit(1);
 
@@ -38,39 +68,43 @@ void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel) {
 	// we subtract the focalLength bc we are looking into the -z axis, to respect the RH-coordinate system
 	auto lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focalLength);
 
+	// for progress tracking
+	std::cout << "imageWidth: " << imageWidth << " imageHeight: " << imageHeight << "\n" << std::endl;
+	int printProgress[100] = {};
+
 	//Draws a gradient from blue to green
 	for (int i = 0; i < imageWidth; i++) {
 		for (int j = imageHeight-1; j > 0; j--) {
-			//color.rgbRed = 0;
-			//color.rgbGreen = (double)i / WIDTH * 255.0;
-			//color.rgbBlue = (double)j / HEIGHT * 255.0;
 
-			//color pixel_color(0.25, double(i) / imageWidth, double(j) / imageHeight);
+			// print progress
+			PrintProgress(i, j, imageWidth, imageHeight, printProgress);
 
+			// uv mappings of pixels
 			auto u = double(i) / (imageWidth-1);
 			auto v = double(j) / (imageHeight-1);
 			ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
 			color pixel_color = ray_color(r);
 
+			// converts our color object to RGBQUAD for FreeImage
 			write_color(std::cout, pixel_color, freeimage_color);
 
+			// a pointer needs to be passed to the color struct
 			FreeImage_SetPixelColor(bitmap, i, j, freeimage_color);
-			cout << "pixel: " << i << ", " << j << endl;
-			// Notice the & operator on color; this passes the pointer to the color struct
 		}
 	}
+	std::cout << "\nDone.\n";
 
-	if (FreeImage_Save(FIF_PNG, bitmap, "test.png", 0)) cout << "Image successfully saved!" << endl;
+	if (FreeImage_Save(FIF_PNG, bitmap, "test.png", 0)) std::cout << "Image successfully saved!" << std::endl;
 
-	cout << "FreeImage_" << FreeImage_GetVersion() << "\n";
-	cout << FreeImage_GetCopyrightMessage() << "\n\n";
+	std::cout << "FreeImage_" << FreeImage_GetVersion() << "\n";
+	std::cout << FreeImage_GetCopyrightMessage() << "\n\n";
 	FreeImage_DeInitialise();
 }
 
 int main() {
 
 	const double aspectRatio = 16.0 / 9.0;
-	const int imageWidth = 25;
+	const int imageWidth = 5000;
 	const int bitsPerPixel = 24;
 	Rasterize(aspectRatio, imageWidth, bitsPerPixel);
 }
--- CSE168/hw1/RayTracer/src/color.h
+++ CSE168/hw1/RayTracer/src/color.h
@@ -12,9 +12,10 @@ void write_color(std::ostream& out, color pixel_color, RGBQUAD *color) {
     color->rgbGreen = pixel_color.y() * 255.999;
     color->rgbBlue = pixel_color.z() * 255.999;
 
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    // prints out color value
+    //out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+    //    << static_cast<int>(255.999 * pixel_color.y()) << ' '
+    //    << static_cast<int>(255.999 * pixel_color.z()) << '\n';
 }
 
 #endif
