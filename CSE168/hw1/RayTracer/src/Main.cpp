e7f4ce511af4bad4acc228a677d559d00acfc8b3
Vivian Leung <vivz753@gmail.com>
2020-12-12 12:01:45 -0800

    add CSE168 hw1-submissionscenes, add file parser for scene files

--- CSE168/hw1/RayTracer/src/Main.cpp
+++ CSE168/hw1/RayTracer/src/Main.cpp
@@ -6,6 +6,16 @@
 #include "sphere.h"
 #include "hittable_list.h"
 
+#include <sstream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel);
+void Rasterize(int, int);
+
 double hit_sphere(const point3& center, double radius, const ray& r)
 {
 	// ray from center of sphere to eye
@@ -55,6 +65,12 @@ void PrintProgress(int i, int j, int imageWidth, int imageHeight, int progressAr
 	}
 }
 
+void Rasterize(int imageWidth, int imageHeight) {
+	double aspectRatio = imageWidth / imageHeight;
+	int bpp = 24;
+	Rasterize(aspectRatio, imageWidth, bpp);
+}
+
 void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel) {
 
 	// Image
@@ -72,8 +88,10 @@ void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel) {
 	// World
 
 	hittable_list world;
+	world.add(make_shared<sphere>(point3(1, 0, -1), .5));
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
 
 	// Camera
 
@@ -123,10 +141,146 @@ void Rasterize(double aspectRatio, int imageWidth, int bitsPerPixel) {
 	FreeImage_DeInitialise();
 }
 
+// Taken from CS167X hw2
+// Function to read the input data values
+// Use is optional, but should be very helpful in parsing.  
+bool readvals(stringstream& s, const int numvals, float* values)
+{
+    for (int i = 0; i < numvals; i++) {
+        s >> values[i];
+        if (s.fail()) {
+            std::cout << "Failed reading value " << i << " will skip\n";
+            return false;
+        }
+    }
+    return true;
+}
+
+void ReadFile(const char* filename) {
+    string str, cmd;
+    ifstream in;
+    in.open(filename);
+
+
+    if (in.is_open()) {
+
+        // I need to implement a matrix stack to store transforms.  
+        // This is done using standard STL Templates 
+        //stack <mat4> transfstack;
+        //transfstack.push(mat4(1.0));  // identity
+
+		std::cout << "Reading file " << filename << std::endl;
+
+        
+        while (getline(in, str)) {
+
+            if ((str.find_first_not_of(" \t\r\n") != string::npos) && (str[0] != '#')) {
+                // Ruled out comment and blank lines 
+
+                stringstream s(str);
+                s >> cmd;
+
+				std::cout << str << std::endl;
+
+
+                int i;
+                float v[10]; // Position and color for light, colors for others
+                // Up to 10 params for cameras.  
+                bool validinput; // Validity of input 
+
+				// Image size
+				if (cmd == "size") {
+					// width, height
+				}
+				// Image file output
+				else if (cmd == "output") {
+					// "name.png"
+
+				}
+				// Camera
+				else if (cmd == "camera") {
+					// lookFrom x, y, z; lookAt x, y, z; R, G, B, A
+					if (readvals(s, 10, v)) {
+						vec3 lookFrom = vec3(v[0], v[1], v[2]);
+						vec3 lookAt = vec3(v[3], v[4], v[5]); // center of image
+						vec3 up = unit_vector(vec3(v[6], v[7], v[8]));
+
+						int fovy = v[9];
+					}
+				}
+				// Lights
+				else if (cmd == "light") {
+
+				}
+				else if (cmd == "point") {
+
+				}
+				// Materials
+				else if (cmd == "ambient") {
+
+				}
+				else if (cmd == "emission") {
+
+				}
+				else if (cmd == "diffuse") {
+
+				}
+				else if (cmd == "shininess") {
+
+				}
+				else if (cmd == "specular") {
+
+				}
+				// Matrix access
+				else if (cmd == "pushTransform") {
+
+				}
+				else if (cmd == "popTransform") {
+
+				}
+				// Transformation matrices
+				else if (cmd == "translate") {
+
+				}
+				else if (cmd == "scale") {
+
+				}
+				else if (cmd == "rotate") {
+
+				}
+				// Geometry
+				else if (cmd == "sphere") {
+				
+				}
+				else if (cmd == "tri") {
+
+				}
+				else if (cmd == "maxverts") {
+
+				}
+				else if (cmd == "vertex") {
+
+				}
+
+				else {
+					cerr << "Unknown Command: " << cmd << "Skipping" << std::endl;
+				}
+            }
+        }
+		in.close();
+    }
+    else {
+        cerr << "Unable to Open Input Data File " << filename << "\n";
+    }
+}
+
+
 int main() {
 
 	const double aspectRatio = 16.0 / 9.0;
 	const int imageWidth = 500;
 	const int bitsPerPixel = 24;
-	Rasterize(aspectRatio, imageWidth, bitsPerPixel);
+	//Rasterize(aspectRatio, imageWidth, bitsPerPixel);
+	string filename = "C:/dev/vivz753/ComputerGraphics/CSE168/hw1/RayTracer/src/homework1-submissionscenes/scene4-diffuse.test";
+	ReadFile(filename.c_str());
 }
